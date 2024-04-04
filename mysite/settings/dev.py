482608d8c9f1bcfe359c765e0ad5f64d9ff154aa
Ben Wilson <ben.wilson@devblue.com>
2024-04-03 18:14:57 -0700

    Initial commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-$87ds^h*ns2p*^odtpmyces5%f_zb%^5^fm7m-6vxuyl2)7-2$"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
